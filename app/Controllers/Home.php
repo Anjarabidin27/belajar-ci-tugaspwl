72beadb0885372f1840bf11f66bb2f0e9b1fbf26
Anjar Abidin <anjarbdn@gmail.com>
2025-07-06 00:49:27 +0700

    Initial commit

---
+++ app/Controllers/Home.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\ProductModel; 
+
+class Home extends BaseController
+{
+    function  __construct(){
+        $this->product = new ProductModel();
+        helper('form');
+        helper('number');
+    }
+
+
+    public function index(): string
+    {   
+        $product = $this->product->findAll();
+        $data['product'] = $product;        
+
+        return view('v_home', $data);
+    }
+
+    public function faq()
+    {
+        return view('v_faq');
+    }
+
+    public function profile()
+    {
+        return view('v_profile');
+    }
+
+    public function contact()
+    {
+        return view('v_contact');
+    }
+}
